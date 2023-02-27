de593dab368b5e016c2ff478482b725c8ca676f9
Jakub Bednarski <64811286+SuperrMurlocc@users.noreply.github.com>
2023-02-27 17:37:41 +0100

    Refactoring main.py

--- lab1/main.py
+++ lab1/main.py
@@ -5,7 +5,7 @@ import sys
 def my_printf(format_string,param):
     print(format_string.replace('#k', param.swapcase()))
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[ i + 1].rstrip())
