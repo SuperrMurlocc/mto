f68c21cca690d8415ef54686cbf5f30bb00427d5
Jakub Bednarski <64811286+SuperrMurlocc@users.noreply.github.com>
2023-03-06 17:22:30 +0100

    ostatnia poprawka main.py

--- lab2/main.py
+++ lab2/main.py
@@ -8,15 +8,15 @@ import re
 def my_printf(format_string, param):
     match = re.search("#(\.\d+)?k", format_string)
 
-    if not match:
+    if not match:  # jeśli nie ma #.Xk ani #k to printnij normalnie.
         print(format_string)
         return
 
     replacement = format_string[match.start() : match.end()]
 
     max_chars = len(param)
-    if match.group(1):
-        max_chars = int(match.group(1)[1:])
+    if match.group(1):  # jeśli wyłapał .X
+        max_chars = int(match.group(1)[1:])  # zczytaj bez kropki!
 
     print(format_string.replace(replacement, param.swapcase()[:min(len(param), max_chars)]))
 
