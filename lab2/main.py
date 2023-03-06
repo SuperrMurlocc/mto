756bf4d66afb126c805d0afcecc53da91947914a
Jakub Bednarski <64811286+SuperrMurlocc@users.noreply.github.com>
2023-03-06 17:19:24 +0100

    poprawa main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
+
 import sys
 import re
 
-def my_printf(format_string,param):
+
+def my_printf(format_string, param):
     match = re.search("#(\.\d+)?k", format_string)
 
     if not match:
         print(format_string)
         return
 
-    replacement = format_string[match.start():match.end()]
+    replacement = format_string[match.start() : match.end()]
 
     max_chars = len(param)
     if match.group(1):
         max_chars = int(match.group(1)[1:])
 
-    print(format_string.replace(replacement, param.swapcase()[:min(len(param),max_chars)]))
+    print(format_string.replace(replacement, param.swapcase()[:min(len(param), max_chars)]))
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
